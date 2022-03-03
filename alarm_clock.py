0187dbf6490f09475210b01b74e7f3ff98125b40
Amanda <aclaas522@gmail.com>
2022-03-03 10:44:38 -0600

    finished alarmclock

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,18 +1,17 @@
-from signal import alarm
-
-
-class Alarm_clock:
-
+class Alarm_Clock:
     def __init__(self):
         self.current_time='12:00'
-        self.alarm_is_on=True
         self.alarm_time='6:00'
+        self.alarm_is_off=True
     
-    def change_time(self, current_time):
-        self.current_time += current_time
-
+    def change_time(self, new_time):
+        self.current_time = new_time
+        print(self.current_time)
     def set_alarm(self, alarm_time):
-        self.alarm_time == alarm_time
-        
-    def silence_alarm(self, alarm_is_on):
-        self.alarm_is_on
+        self.alarm_time = alarm_time
+        print(self.alarm_time)  
+    def toggle_alarm(self):
+        if self.alarm_is_off==True:
+            self.alarm_is_off=False
+        else:
+            self.alarm_is_off=True
---
+++ main.py
@@ -0,0 +1,8 @@
+from alarm_clock import Alarm_Clock
+
+amandas_alarm_clock=Alarm_Clock('7:00')
+bobs_alarm_clock=Alarm_Clock('6:45')
+
+print(amandas_alarm_clock.current_time)
+print(amandas_alarm_clock.alarm_time)
+print(bobs_alarm_clock.alarm_time)
